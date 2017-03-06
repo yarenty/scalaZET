5ca60613ffff86e8239e425a6045b451493be671
yarenty <yarenty@gmail.com>
2017-03-06 22:55:41 +0000

    cleaning visibility for other Learns

--- src/main/scala/Learn1.scala
+++ src/main/scala/Learn1.scala
@@ -1,28 +1,26 @@
 /**
   * Created by yarenty on 06/03/2017.
   */
-trait Monoid[A] {
-  def mappend(a1: A, a2: A): A
-
-  def mzero: A
-}
-
-object Monoid {
-  implicit val IntMonoid: Monoid[Int] = new Monoid[Int] {
-    def mappend(a: Int, b: Int): Int = a + b
-
-    def mzero: Int = 0
-  }
-  implicit val StringMonoid: Monoid[String] = new Monoid[String] {
-    def mappend(a: String, b: String): String = a + b
-
-    def mzero: String = ""
-  }
-}
-
-
 object Learn1 {
 
+  trait Monoid[A] {
+    def mappend(a1: A, a2: A): A
+
+    def mzero: A
+  }
+
+  object Monoid {
+    implicit val IntMonoid: Monoid[Int] = new Monoid[Int] {
+      def mappend(a: Int, b: Int): Int = a + b
+
+      def mzero: Int = 0
+    }
+    implicit val StringMonoid: Monoid[String] = new Monoid[String] {
+      def mappend(a: String, b: String): String = a + b
+
+      def mzero: String = ""
+    }
+  }
 
   def sum[A: Monoid](xs: List[A]): A = {
     val m = implicitly[Monoid[A]]
