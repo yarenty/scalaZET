0a20180a052aa8ee146d1ca60e32e2c1cc46380c
yarenty <yarenty@gmail.com>
2017-03-06 22:46:12 +0000

    and implicit

--- src/main/scala/Learn1.scala
+++ src/main/scala/Learn1.scala
@@ -3,27 +3,32 @@
   */
 trait Monoid[A] {
   def mappend(a1: A, a2: A): A
+
   def mzero: A
 }
 
+
+
 object IntMonoid extends Monoid[Int] {
   def mappend(a: Int, b: Int): Int = a + b
+
   def mzero: Int = 0
 }
+
+
 
 
 
 object Learn1 {
 
 
-
-
-
-  def sum[A](xs: List[A], m: Monoid[A]): A = xs.foldLeft(m.mzero)(m.mappend)
+  def sum[A](xs: List[A])(implicit m: Monoid[A]): A = xs.foldLeft(m.mzero)(m.mappend)
 
   def main(args: Array[String]): Unit = {
 
-    val out = sum(List(1, 2, 3, 4), IntMonoid)
+    implicit val intMonoid = IntMonoid
+
+    val out = sum(List(1, 2, 3, 4))
 
     println(out)
   }
