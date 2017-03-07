4df40aebdfa98c9893e6f2e9775e267f4eb3b9db
yarenty <yarenty@gmail.com>
2017-03-07 22:10:35 +0000

    typeclasses 102

---
+++ src/main/scala/Learn5.scala
@@ -0,0 +1,36 @@
+/**
+  * Created by yarenty on 07/03/2017.
+  */
+
+import scalaz.Scalaz._
+import scalaz._
+
+/**
+  * typeclases 102
+  */
+object Learn5 {
+//
+//  sealed trait TrafficLight
+//  case object Red extends TrafficLight
+//  case object Yellow extends TrafficLight
+//  case object Green extends TrafficLight
+
+
+  implicit val TrafficLightEqual: Equal[TrafficLight] = Equal.equal(_ == _)
+
+  case class TrafficLight(name: String)
+  val red = TrafficLight("red")
+  val yellow = TrafficLight("yellow")
+  val green = TrafficLight("green")
+
+
+
+
+  def main(args: Array[String]): Unit = {
+
+    //Red === Yellow
+    println( red === yellow )
+
+
+  }
+}
