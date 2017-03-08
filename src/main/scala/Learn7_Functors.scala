f91fd85b453b1d4bfc4ed6b332c1c185f23f3327
yarenty <yarenty@gmail.com>
2017-03-08 22:05:43 +0000

    functors... in new scalaz there is no fmap

---
+++ src/main/scala/Learn7_Functors.scala
@@ -0,0 +1,44 @@
+/**
+  * Created by yarenty on 08/03/2017.
+  */
+import scalaz._
+import Scalaz._
+
+object Learn7_Functors {
+
+  def main(args: Array[String]): Unit = {
+
+    println( (1, 2, 3) map {_ + 1} )
+
+    val a = ((x: Int) => x + 1) map {_ * 7}
+
+    println( a(3))
+
+//
+//    final def map[B](f: A => B): F[B] = F.map(self)(f)
+//
+//    fmap :: (a -> b) -> (r -> a) -> (r -> b)
+//
+//    val u = fmap (*3) (+100) 1
+
+
+//    :t map (*3) (+100) 1
+
+
+    println( List(1, 2, 3) map {3*} )
+
+
+    val u = Functor[List].lift {(_: Int) * 2}
+
+    println(u(List(3)))
+
+    println(List(1, 2, 3) >| "x")
+    println(List(1, 2, 3) as "x")
+    println(List(1, 2, 3).fpair)
+    println(List(1, 2, 3).strengthL("x"))
+    println(List(1, 2, 3).strengthR("x"))
+    println(List(1, 2, 3).void)
+
+  }
+
+}
