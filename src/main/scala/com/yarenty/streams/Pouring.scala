f96ca1d972d0bf709dd81320369842bed0577736
Jaro <yarenty@gmail.com>
2017-03-21 14:40:24 +0000

    pouring problem #2 changes and paths

--- src/main/scala/com/yarenty/streams/Pouring.scala
+++ src/main/scala/com/yarenty/streams/Pouring.scala
@@ -1,5 +1,7 @@
 package com.yarenty.streams
 
+import scala.collection.mutable
+
 /**
   * Created by yarenty on 21/03/17.
   */
@@ -11,10 +13,22 @@ class Pouring(capacity:Vector[Int]) {
   val initialState:Vector[Int] = capacity map ( x => 0)
 
   //Moves
-  trait Move
-   case class Empty(glass:Int) extends Move
-   case class Pour(from:Int, to:Int) extends Move
-   case class Fill(glass:Int) extends Move
+  trait Move {
+    def change(state:State):State
+  }
+   case class Empty(glass:Int) extends Move {
+     override def change(state: State) = state updated (glass,0)
+   }
+   case class Pour(from:Int, to:Int) extends Move {
+     override def change(state: State) = {
+       val amount = state(from) min (capacity(to) - state(to))
+       state updated (from, state(from) - amount)  updated (to, state(to) + amount)
+
+     }
+   }
+   case class Fill(glass:Int) extends Move {
+     override def change(state: State) = state updated ( glass, capacity(glass))
+   }
 
   val glasses:Range = capacity.indices
 
@@ -23,5 +37,18 @@ class Pouring(capacity:Vector[Int]) {
     ( for (g <- glasses) yield Fill(g)) ++
     ( for (from <- glasses; to <- glasses if from!=to ) yield Pour(from,to))
 
+
+  //paths
+  class Path(history: List[Move]) { // last move in path is first in history
+
+    def endState: State = trackState(history)
+
+    private def trackState(xs: List[Move]): State = xs match {
+      case Nil => initialState
+      case move :: xs1 => move change trackState(xs1)
+    }
+
+
+  }
 
 }
